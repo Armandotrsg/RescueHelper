e66bfd1aa040fab09c8991a8a08fb0bf23808e4f
Armando Terrazas <mandotrs@icloud.com>
2022-11-21 20:07:57 -0600

    Muestra la vsensación de la vibración

--- js/index.js
+++ js/index.js
@@ -34,7 +34,38 @@ let vibraciones = document.getElementById("vibraciones");
 
 const unsub = onSnapshot(doc(db, "Edificio1", "Salon1"), (doc) => {
     peopleCounter.innerHTML = doc.data().people;
-    vibraciones.innerHTML = doc.data().vibrations;
+    let sensacion = "";
+    //Round doc.data().vibrations to 4 decimals and absolute value
+    let vib = Math.abs(Math.round(doc.data().vibrations * 10000) / 10000);
+    let text = "text-success";
+    if (vib < 0.0017) {
+        sensacion = "Ninguna";
+    } else if (vib < 0.014) {
+        sensacion = "Débil";
+    } else if (vib < 0.039) {
+        sensacion = "Ligera";
+        text = "text-warning";
+    } else if (vib < 0.092) {
+        sensacion = "Moderada";
+        text = "text-warning";
+    } else if (vib < 0.18) {
+        sensacion = "Fuerte";
+        text = "text-danger";
+    } else if (vib < 0.34) {
+        sensacion = "Muy fuerte";
+        text = "text-danger";
+    } else if (vib < 0.65) {
+        sensacion = "Severa";
+        text = "text-danger";
+    } else if (vib < 1.24) {
+        sensacion = "Muy severa";
+        text = "text-danger";
+    } else {
+        sensacion = "Extrema";
+        text = "text-danger";
+    }
+    vibraciones.innerHTML = String(vib) + " g" + "&nbsp&nbsp&nbspSensación: " + sensacion;
+    vibraciones.className = "text-center mb-5" + " " + text;
 });
 
 /* // Initialize Firebase
